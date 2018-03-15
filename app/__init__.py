c622c59fb22ac6610cc955035a212c754bd14f15
En Zhou <en.zhou@opendna.ai>
2018-03-16 01:49:07 +0200

    Finished up to employee

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python
-
+from flask_bootstrap import Bootstrap
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_migrate import Migrate
 
 # local imports
 from config import app_config
-from flask_login import LoginManager
-from flask_migrate import Migrate
 
 # db variable initialization
 db = SQLAlchemy()
@@ -16,18 +16,25 @@ def create_app(config_name):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
+
+    Bootstrap(app)
     db.init_app(app)
 
     login_manager.init_app(app)
     login_manager.login_message = "You must be logged in to access this page."
-    #  login_manager.login_view("auth.login")
-    # temporary route
-    #  @app.route('/')
-
-    #  def hello_world():
-    #      return 'Hello, World!'
-    #
+    login_manager.login_view = "auth.login"
     migrate = Migrate(app, db)
+
     from app import models
+
+    from .admin import admin as admin_blueprint
+    app.register_blueprint(admin_blueprint, url_prefix='/admin')
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint)
+
+    from .home import home as home_blueprint
+    app.register_blueprint(home_blueprint)
+
 
     return app
--- app/models.py
+++ app/models.py
@@ -1,4 +1,3 @@
-#!venv/bin/python
 
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
