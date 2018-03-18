afc80910a51172e4b66f3690c173f20a3488f9fe
En Zhou <en.zhou@opendna.ai>
2018-03-19 00:34:48 +0200

    Add error pages and test

--- config.py
+++ config.py
@@ -3,21 +3,35 @@ class Config(object):
     Common configurations
     """
 
-# Put any configurations here that are common across all environments
+    DEBUG = True
+
+
 class DevelopmentConfig(Config):
     """
     Development configurations
     """
-    DEBUG = True
+
     SQLALCHEMY_ECHO = True
+
 
 class ProductionConfig(Config):
     """
     Production configurations
     """
+
     DEBUG = False
+
+
+class TestingConfig(Config):
+    """
+    Testing configurations
+    """
+
+    TESTING = True
+
 
 app_config = {
     'development': DevelopmentConfig,
-    'production': ProductionConfig
+    'production': ProductionConfig,
+    'testing': TestingConfig
 }
